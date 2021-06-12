df59686d10aae5c49b1e5ebeeb624ad68b7a0fbe
Mohammed Owez <59692344+mohammedowez@users.noreply.github.com>
2021-06-12 20:04:31 +0530

    Create Question6.cpp

---
+++ Question6/Question6.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+int solve()
+{ 
+// taking the input 
+	long long  C,D,L;
+	cin>>C>>D>>L;
+	
+	
+        int  tmp = 0;
+       
+	    //checking the mainimum cats and dogs 
+        long long  mi = 4*(D + max(tmp, C-(2*D)));
+        
+        // taking all the cats and dogs 
+        long long  ma = 4*(C+D);
+        
+        //  check the legs are divided by 4 
+        // and then checking all the legs with minimum and maximum 
+        // if all of them are true then output yes else no
+        if(L%4==0 && L<=ma && L>=mi)
+            cout<<"yes\n";
+        else
+            cout<<"no\n";
+}
+
+
+int main()
+{
+	 // input the test case
+    long long int T;
+    cin>>T;
+    
+    while(T--)
+	 { 
+	 
+	 solve();
+        
+    }
+    return 0;
+}
